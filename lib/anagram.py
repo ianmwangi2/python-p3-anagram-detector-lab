16510c9f034e86436bf0e8b1f6dbecc78c83b2fb
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-08-29 19:47:20 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words_list):
+        def is_anagram(word1, word2):
+            return sorted(word1) == sorted(word2)
+        
+        return [word for word in words_list if is_anagram(self.word, word)]
+
+    def __str__(self):
+        return f"Anagram(word={self.word})"
